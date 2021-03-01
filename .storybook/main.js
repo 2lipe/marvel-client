13cc8071cd1729bf83f6ef0b02ddbdf3aac9c2ec
Felipe Vieira <felipe.svfx@gmail.com>
2021-03-01 08:56:26 -0300

    chore: add generators with plop

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../src/**/*.stories.(js|jsx|ts|tsx)'],
+  stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
 };
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export const Header = () => (
+  <S.Wrapper>
+    <h1>Header</h1>
+  </S.Wrapper>
+);
