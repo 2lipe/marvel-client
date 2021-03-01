cb61176f72b83537ddbb930df761595c4b3508e5
Felipe Vieira <felipe.svfx@gmail.com>
2021-03-01 08:10:35 -0300

    chore: add eslint/storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+import { addDecorator } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
+import { ThemeProvider } from 'styled-components'
+
+import theme from 'styles/theme'
+import GlobalStyles from 'styles/global'
+
+addDecorator(withNextRouter())
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
---
+++ src/test.test.tsx
@@ -0,0 +1,7 @@
+describe('sum', () => {
+  it('1 to be equal 1', () => {
+    expect(1).toBe(1);
+  });
+});
+
+export {};
